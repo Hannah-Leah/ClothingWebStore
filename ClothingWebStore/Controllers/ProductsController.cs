7e8322dcfc665f60bcf00539f04aa57ce9f8436b
Hanie <Hanie@HANNAHLEAH>
2026-04-17 09:54:03 +0200

    you can now upload multiple pictures for a product!

--- ClothingWebStore/Controllers/ProductsController.cs
+++ ClothingWebStore/Controllers/ProductsController.cs
@@ -66,13 +66,16 @@ namespace ClothingWebStore.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+
+
+       /*
         public async Task<IActionResult> Create([Bind("ProductId,Name,Description,ItemNumber,Price,Stock,IsArchived,CategoryId,ManufacturerId")] Product product,
     IFormFile imageFile)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(product);
-                await _context.SaveChangesAsync(); 
+                await _context.SaveChangesAsync();
 
                 // HANDLE IMAGE UPLOAD
                 if (imageFile != null && imageFile.Length > 0)
@@ -93,6 +96,55 @@ namespace ClothingWebStore.Controllers
                     };
 
                     _context.ProductImages.Add(productImage);
+                    await _context.SaveChangesAsync();
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", product.CategoryId);
+            ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
+
+            return View(product);
+        }
+       */
+
+
+        public async Task<IActionResult> Create([Bind("ProductId,Name,Description,ItemNumber,Price,Stock,IsArchived,CategoryId,ManufacturerId")] Product product,
+    List<IFormFile> imageFiles)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+
+                // HANDLE IMAGE UPLOAD
+                if (imageFiles != null && imageFiles.Any())
+                {
+                    foreach (var imageFile in imageFiles)
+                    {
+                        if (imageFile.Length > 0)
+                        {
+                            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                            var filePath = Path.Combine(
+                                Directory.GetCurrentDirectory(),
+                                "wwwroot/images",
+                                fileName
+                            );
+
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                await imageFile.CopyToAsync(stream);
+                            }
+
+                            _context.ProductImages.Add(new ProductImage
+                            {
+                                ProductId = product.ProductId,
+                                ImageUrl = "/images/" + fileName
+                            });
+                        }
+                    }
+
                     await _context.SaveChangesAsync();
                 }
 
@@ -129,6 +181,9 @@ namespace ClothingWebStore.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+
+        /*
+         
         public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,Description,ItemNumber,Price,Stock,IsArchived,CategoryId,ManufacturerId")] Product product)
         {
             if (id != product.ProductId)
@@ -160,6 +215,91 @@ namespace ClothingWebStore.Controllers
             ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
             return View(product);
         }
+         
+          */
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,Description,ItemNumber,Price,Stock,IsArchived,CategoryId,ManufacturerId")] Product product, List<IFormFile> imageFiles)
+        {
+
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", product.CategoryId);
+                ViewData["ManufacturerId"] = new SelectList(_context.Manufacturers, "ManufacturerId", "Name", product.ManufacturerId);
+                return View(product);
+            }
+
+            try
+            {
+              
+                var existingProduct = await _context.Products
+                    .Include(p => p.ProductImages)
+                    .FirstOrDefaultAsync(p => p.ProductId == id);
+
+                if (existingProduct == null)
+                {
+                    return NotFound();
+                }
+
+                
+                existingProduct.Name = product.Name;
+                existingProduct.Description = product.Description;
+                existingProduct.ItemNumber = product.ItemNumber;
+                existingProduct.Price = product.Price;
+                existingProduct.Stock = product.Stock;
+                existingProduct.IsArchived = product.IsArchived;
+                existingProduct.CategoryId = product.CategoryId;
+                existingProduct.ManufacturerId = product.ManufacturerId;
+
+                // Add new uploaded images
+                if (imageFiles != null && imageFiles.Any())
+                {
+                    foreach (var imageFile in imageFiles)
+                    {
+                        if (imageFile.Length > 0)
+                        {
+                            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+                            var filePath = Path.Combine(
+                                Directory.GetCurrentDirectory(),
+                                "wwwroot/images",
+                                fileName
+                            );
+
+                            using (var stream = new FileStream(filePath, FileMode.Create))
+                            {
+                                await imageFile.CopyToAsync(stream);
+                            }
+
+                            existingProduct.ProductImages.Add(new ProductImage
+                            {
+                                ProductId = existingProduct.ProductId,
+                                ImageUrl = "/images/" + fileName
+                            });
+                        }
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(product.ProductId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
 
 
         // GET: Products/Delete/5
